1debf64883fdb7cdb8767edd4d90907f4c776bab
“lakshmikant” <fullstacktrainer4u@gmail.com>
2025-01-23 07:44:35 +0530

    update console

--- lesson12_aws_ec2_private_server/telecom_private_server/app.js
+++ lesson12_aws_ec2_private_server/telecom_private_server/app.js
@@ -6,7 +6,7 @@ require('dotenv').config(); // Load environment variables from .env
 const winston = require("winston");
 
 const logger = winston.createLogger({
-  transports: [new winston.transports.Console()],
+    transports: [new winston.transports.Console()],
 });
 
 logger.error("Error message");
@@ -25,13 +25,13 @@ const metricsData = [
 
 // API Endpoint for Metrics
 app.get('/metrics', (req, res) => {
-    console.log("Metrics API called");
+    console.log("Metrics API called" + new Date());
     res.json(metricsData);
 });
 
 // API Endpoint for Subscriptions
 app.get('/subscriptions', (req, res) => {
-    console.log("Subscriptions API called");
+    console.log("Subscriptions API called" + new Date());
     const subscriptionData = [
         { type: 'New', count: 50, revenue: 5000 },
         { type: 'Renewals', count: 70, revenue: 7000 },
@@ -41,7 +41,7 @@ app.get('/subscriptions', (req, res) => {
 
 // API Endpoint for Payments
 app.get('/payments', (req, res) => {
-    console.log("Payments API called");
+    console.log("Payments API called" + new Date());
     const paymentData = [
         { transactionCount: 100, transactionAmount: 20000, gateway: 'PayPal' },
     ];
@@ -59,7 +59,7 @@ const networkData = [
 
 // Network API Endpoint
 app.get('/network', (req, res) => {
-    console.log("Network API called");
+    console.log("Network API called" + new Date());
     try {
         res.json(networkData);
     } catch (error) {
