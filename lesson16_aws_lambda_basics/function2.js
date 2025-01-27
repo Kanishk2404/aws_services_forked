9e92209149d79583cd57008c89e89005a067b410
“lakshmikant” <lakshmikant.lpu@gmail.com>
2025-01-27 07:35:40 +0530

    changes

--- lesson16_aws_lambda_basics/function2.js
+++ lesson16_aws_lambda_basics/function2.js
@@ -84,3 +84,18 @@ export const handler = async (event) => {
 //         };
 //     }
 // };
+
+
+// {
+//     "Version": "2012-10-17",
+//     "Statement": [
+//       {
+//         "Effect": "Allow",
+//         "Action": [
+//           "sns:Publish"
+//         ],
+//         "Resource": "*"
+//       }
+//     ]
+//   }
+  
